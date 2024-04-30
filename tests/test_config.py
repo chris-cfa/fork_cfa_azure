1f6535b32d6f4edbd5d2dcfbb36c51f2a5cf75f0
Ryan Raasch <xng3@cdc.gov>
2024-04-30 13:15:29 -0700

    update config and test

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,7 +12,7 @@ class TestReadConfig(unittest.TestCase):
         expected = {'Authentication': {'subscription_id': 'sub_id', 'resource_group': 'resource_group',
                                        'user_assigned_identity': 'user_assigned_identity', 'tenant_id': 'tenant_id', 
                                        'client_id': 'client_id', 'principal_id': 'principal_id', 'application_id': 'application_id', 
-                                       'vault_url': 'https://cfa-common-adf-prd.vault.azure.net/', 
+                                       'vault_url': 'vault_url', 
                                        'vault_sp_secret_id': 'CFA-SECRET_ID', 'vault_sa_secret_id':
                                        'CFA-SA_SECRET_ID', 'vault_ab_secret_id': 'CFA-AB_SECRET_ID', 'subnet_id': 'subnet_id'}, 
                     'Batch': {'batch_account_name': 'batch_account_name', 'batch_url': 'batch_url', 'batch_service_url': 'batch_service_url', 
